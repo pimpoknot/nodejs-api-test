14585870419f1146d8afb87dabb46402bd9ae014
Marcos Maverick <marquinhopimpo@gmail.com>
2021-07-30 19:06:03 -0300

    admin validator

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,8 +3,8 @@ import { CreateUserService } from '../services/CreateUserService'
 
 
 class CreateUserController {
-
     async handle(request: Request, response: Response) {
+     
         const { name, email, admin } = request.body
 
         const createUserService = new CreateUserService();
