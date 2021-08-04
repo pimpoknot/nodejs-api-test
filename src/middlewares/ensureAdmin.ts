b647be63039d803704ddf637f3baa954ee0289e9
Marcos Maverick <marquinhopimpo@gmail.com>
2021-08-04 14:02:48 -0300

    ajuste no middleware validando tags

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -2,10 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 
 export function ensureAdmin (request: Request, response: Response, next: NextFunction) {
 
-    const admin = false;
+    const admin = true;
 
     if(admin) {
-        return next;
+        return next();
     }
 
     return response.status(401).json({
